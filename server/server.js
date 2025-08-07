16ddc3f3d0256587151ff4e88d097b0b5a655982
Mushimaaro <mushimaaro@gmail.com>
2025-08-07 13:01:07 +0800

    Update server.js Port

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ import roleRouter from "./routers/roleRouter.js";
 import clientRouter from "./routers/clientRouter.js";
 
 const app = express();
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 
 // Cors
